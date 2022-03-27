7eb454d162d065c96c47e16a14dca286dbb9c6a8
ArcCosine <40669+ArcCosine@users.noreply.github.com>
2022-03-27 10:28:55 +0900

    [Add] Add vite with React.

---
+++ go-chi-u-cycle/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Go-chi-u-cycle</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
